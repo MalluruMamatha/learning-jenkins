00e679122b371a0cee3ffa45dc3286510059e223
MalluruMamatha <mamatha.mm939@gmail.com>
2024-07-23 01:09:18 +0530

    jenkins

--- main.tf
+++ main.tf
@@ -38,6 +38,40 @@ module "jenkins_agent" {
 
 } 
 
+resource "aws_key_pair" "nexus" {
+  key_name   = "nexus"
+  #public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAAj+LuGH97RBcpUk7yrNkh964sqB0Az2yjzyCW3gdJW Mammu@DESKTOP-AUVJB9I"
+  public_key = file("~/.ssh/nexus.pub")
+
+  ### you can give the pub key or else pub key file path here
+
+}
+
+
+module "nexus" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "nexus"
+
+  instance_type          = "t3.medium"
+  ami = data.aws_ami.nexus_ami_info.id
+  vpc_security_group_ids = ["sg-02b9c227b04592725"]  
+  subnet_id = "subnet-05957263b02ce2391"
+  key_name = aws_key_pair.nexus.key_name
+  root_block_device = [
+    {
+      volume_type = "gp3"
+      volume_size = 30
+    }
+  ]
+
+  tags = {
+
+    Name = "nexus"
+  }
+
+} 
+
   module "records" {
   source  = "terraform-aws-modules/route53/aws//modules/records"
   version = "~> 3.0"
@@ -62,6 +96,15 @@ zone_name = var.zone_name
       ttl     = 1
       records = [
         module.jenkins_agent.private_ip
+      ]
+       allow_overwrite = true
+    },
+    {
+      name    = "nexus"
+      type    = "A"
+      ttl     = 1
+      records = [
+        module.nexus.private_ip
       ]
        allow_overwrite = true
     }
